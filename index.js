64ff31f618e3999e3cee13911cdb856a7e1353ae
Iconoclast-70 <phil.mh.milly@gmail.com>
2021-08-24 06:14:33 +0000

    recursive array

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let arrSum = 0;
+  for (const arr of array) {
+    if (Array.isArray(arr)) {
+      arrSum = arrSum + sumItems(arr);
+    } else {
+      arrSum = arrSum + arr;
+    }
+  }
+  return arrSum;
 }
 
 module.exports = sumItems;
